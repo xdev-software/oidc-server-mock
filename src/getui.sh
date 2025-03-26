f016f36c24ddab9823f5a7993c64da3ae7904d03
AB <a.bierler@xdev-software.de>
2025-03-26 16:56:29 +0100

    Adopt frontend changes
    Fixes #34
    
    Note that the repo was archived

--- src/getui.sh
+++ src/getui.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-TAG="6.3.0"
+COMMIT="cf4546f6dcded593ca1dc08a343c599a4ad8917e"
 
-SOURCE="https://github.com/DuendeSoftware/IdentityServer.Quickstart.UI/archive/refs/tags/$TAG.zip"
+SOURCE="https://github.com/DuendeArchive/IdentityServer.Quickstart.UI/archive/$COMMIT.zip"
 curl -L -o ui.zip "$SOURCE"
 
 unzip -d ui ui.zip
@@ -12,10 +12,7 @@ unzip -d ui ui.zip
 [[ -d Pages ]] || mkdir Pages
 [[ -d wwwroot ]] || mkdir wwwroot
 
-cp -r ./ui/IdentityServer.Quickstart.UI-$TAG/Pages/* Pages
-cp -r ./ui/IdentityServer.Quickstart.UI-$TAG/wwwroot/* wwwroot
+cp -r ./ui/IdentityServer.Quickstart.UI-$COMMIT/Pages/* Pages
+cp -r ./ui/IdentityServer.Quickstart.UI-$COMMIT/wwwroot/* wwwroot
 
 rm -rf ui ui.zip
-
-# Workaround until https://github.com/DuendeSoftware/IdentityServer.Quickstart.UI/issues/14 is fixed
-find Pages -name "*.cs" -type f -exec sed -i 's/using IdentityModel;/using Duende.IdentityModel;/g' {} +
